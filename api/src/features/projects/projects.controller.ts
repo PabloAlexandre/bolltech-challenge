5aad266266ca337b184edadf0060132ba585113b
Pablo Salgado <pabloalexandre.nave@gmail.com>
2022-09-24 11:01:15 -0300

    feat: add crud on projects controller

--- api/src/features/projects/projects.controller.ts
+++ api/src/features/projects/projects.controller.ts
@@ -1,51 +1,75 @@
-import { Controller, Delete, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
-import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Delete, Get, Header, Param, Post, Put, Request, UseGuards } from "@nestjs/common";
+import { ApiHeader, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AuthGuard } from "src/utils/guards/AuthGuard";
+import { CreateProjectDTO } from "./dtos/CreateProject";
+import { ProjectsServices } from "./projects.service";
 
 @Controller('/projects')
 @ApiTags('Projects')
 @UseGuards(AuthGuard)
 export class ProjectsController {
 
+  constructor(
+    protected readonly projectsServices: ProjectsServices,
+  ) {}
+
   @Get()
   @ApiOperation({ summary: 'Get all projects' })
   @ApiResponse({ status: 403, description: "Invalid credentials"})
   @ApiResponse({ status: 200, description: "Success Response"})
-  async getProjects() {
-
+  @ApiHeader({ name: 'Authorization', description: 'Authorization Code'})
+  async getProjects(
+    @Request() req: any,
+  ) {
+    return this.projectsServices.getProjects(req.user);
   }
 
   @Get('/:id')
   @ApiOperation({ summary: 'Get specific project' })
   @ApiResponse({ status: 403, description: "Invalid credentials"})
   @ApiResponse({ status: 200, description: "Success Response"})
+  @ApiHeader({ name: 'Authorization', description: 'Authorization Code'})
   async getProject(
-    @Param('id') id: string
+    @Param('id') id: string,
+    @Request() req: any,
   ) {
-
+    return this.projectsServices.getProject(req.user, id);
   }
 
-  @Post('/:id')
+  @Post()
   @ApiOperation({ summary: 'Create a project' })
   @ApiResponse({ status: 403, description: "Invalid credentials"})
   @ApiResponse({ status: 200, description: "Success Response"})
+  @ApiHeader({ name: 'Authorization', description: 'Authorization Code'})
   async createProject(
-    @Param('id') id: string
-  ) {}
+    @Body() project: CreateProjectDTO,
+    @Request() req: any,
+  ) {
+    return this.projectsServices.createProject(req.user.id, project);
+  }
 
   @Put('/:id')
   @ApiOperation({ summary: 'Edit a project' })
   @ApiResponse({ status: 403, description: "Invalid credentials"})
   @ApiResponse({ status: 200, description: "Success Response"})
+  @ApiHeader({ name: 'Authorization', description: 'Authorization Code'})
   async editProject(
-    @Param('id') id: string
-  ) { }
+    @Body() project: CreateProjectDTO,
+    @Param('id') id: string,
+    @Request() req: any,
+  ) { 
+    return this.projectsServices.editProject(req.user.id, id, project);
+  }
 
   @Delete('/:id')
   @ApiOperation({ summary: 'Delete a project' })
   @ApiResponse({ status: 403, description: "Invalid credentials"})
   @ApiResponse({ status: 204, description: "Project deleted"})
+  @ApiHeader({ name: 'Authorization', description: 'Authorization Code'})
   async deleteProject(
-    @Param('id') id: string
-  ) { }
+    @Param('id') id: string,
+    @Request() req: any,
+  ) { 
+    return this.projectsServices.deleteProject(req.user.id, id);
+  }
 }
--- api/src/features/projects/projects.service.ts
+++ api/src/features/projects/projects.service.ts
@@ -1,6 +1,57 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Project } from "./project.entity";
 
 @Injectable()
 export class ProjectsServices {
+  constructor(
+    @InjectRepository(Project)
+    protected readonly projectRepository: Repository<Project>
+  ) {}
 
+  async getAndValidateProject(user, projectId) {
+    const project = await this.projectRepository.findOne({
+      where: { user: { id: user }, id: projectId }
+    });
+
+    if(!project) throw new UnauthorizedException();
+
+    return project;
+  }
+
+  async getProjects(user) {
+    return this.projectRepository.find({
+      where: { user: { id: user.id }  }
+    })
+  }
+
+  async getProject(user, id) {
+    return this.getAndValidateProject(user.id, id);
+  }
+
+  async createProject(user, payload) {
+    const project = this.projectRepository.save({
+      ...payload,
+      user,
+    });
+
+    return project;
+  }
+
+  async editProject(user, id, payload) {
+    const project = await this.getAndValidateProject(user, id);
+
+    await this.projectRepository.update({
+      id,
+    }, payload);
+
+    return {...project, ...payload};
+  }
+
+  async deleteProject(user, id) {
+    const project = await this.getAndValidateProject(user, id);
+
+    await this.projectRepository.delete(project);
+  }
 }
