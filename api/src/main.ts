b5be5fe7b33932e9f9ed7f7c5ac513c80f6af392
Pablo Salgado <pabloalexandre.nave@gmail.com>
2022-09-24 07:55:26 -0300

    feat: add swagger documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  
+  const config = new DocumentBuilder()
+    .setTitle('Bolttech TODO API')
+    .setDescription('The Todo List API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
