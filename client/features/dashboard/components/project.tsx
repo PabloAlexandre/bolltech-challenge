8f4d32588a164292d69fe32161f714d614ce1a56
Pablo Salgado <pabloalexandre.nave@gmail.com>
2022-09-26 01:35:44 -0300

    feat: add footer on project card with input

--- client/features/dashboard/components/project.tsx
+++ client/features/dashboard/components/project.tsx
@@ -1,5 +1,8 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import Tooltip from '@mui/material/Tooltip';
+import { Input } from '../../../components';
+import { useForm } from '../../../hooks/useForm';
 
 interface Props {
   name: string;
@@ -8,16 +11,28 @@ interface Props {
 export const Project = ({
   name,
 }: Props) => {
+  const { getValue, setValue, resetValues } = useForm();
 
   return (
     <div className="card">
       <header>
         <span>Project {name}</span>
         <div className="actions">
-          <EditIcon />
-          <DeleteIcon />
+          <Tooltip title="Edit Project">
+            <EditIcon  />
+          </Tooltip>
+          <Tooltip title="Remove Project">
+            <DeleteIcon />
+          </Tooltip>
         </div>
       </header>
+      <main>
+
+      </main>
+      <footer>
+        <Input name="task" onChange={setValue('task')} value={getValue('task')} /> 
+        <button className="button green min">Add</button>
+      </footer>
     </div>
   )
 
